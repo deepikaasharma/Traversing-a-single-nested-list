bfb952dd83ea50b2023873f596112e4567fae518
Deepika Sharma <deepikaasharma@outlook.com>
2020-08-30 08:13:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+#nums = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# for i in nums:
+#   for j in i:
+#     print(i)
+
+# list1 = [1, 2, 3]
+# list2 = [4, 5, 6]
+
+# for i in list1:
+#   for j in list2:
+#     print(i, j)
+
+#x = [0, 1, 2, 3, 4, 5]
+# y = [5, 6, 7, 8, 9, 10]
+
+# for i in x:
+#     for j in y:
+#         print(i*j)
+
+x = [1, 2, 3]
+y = [3, 2, 1]
+
+for i in x:
+    for j in y:
+        print(i*j)
+
+
+
+
